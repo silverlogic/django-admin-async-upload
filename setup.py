a7167aef516ca3492b139142f98aaf98d4183a0d
Alisson Patricio <eu@alisson.net>
2022-06-28 11:16:17 -0300

    change package name to admin_async_upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-admin-async-upload",
+    name="admin_async_upload",
     version="3.0.5",
     packages=["admin_async_upload"],
     include_package_data=True,
